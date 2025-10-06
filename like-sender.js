5503e3d733b34aa2d8d336f6959c89cf3f1f9e4b
GBreen2 <esdbkmngvbmn@gmail.com>
2025-10-06 12:33:31 +0600

    Update like-sender.js

--- like-sender.js
+++ like-sender.js
@@ -3,7 +3,7 @@
   const sendBtn = document.getElementById('sendBtn');
   const resultBox = document.getElementById('result');
   const LS_KEY = 'ff_like_sender_data';
-  const MAX_DAILY = 3;
+  const MAX_DAILY = 5;
   const SITE_TOKEN = 'Panzer@321'; // site token
   const CHECK_API  = 'https://blogger-cheak2.vercel.app/api/check';
   const LIKE_API   = 'https://apiblogproxy.vercel.app/api/like';
